adfc59a64465a135b4153a425d3a40f7315dcb3c
srjchsv <srjchsv@gmail.com>
2023-04-26 19:00:42 +0400

    refactor: optimized

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(find . -type f -name "*.yaml" -o -name "*.yml")
+for file in $(find . -type f -name "*.yaml" -o -name "*.yml") -not -path "./manifests/ingress/ingress.yaml"
 do
   kubectl apply -f "$file"
 done
